85920f9b84bad9e8edde5faf1f9ff8d408373ccf
Kai Song <kaisong2@illinois.edu>
2017-10-06 22:05:32 -0500

    Made a change on one class. If cache works, only 5 tests will run.

--- observer/src/main/java/com/iluwatar/observer/Orcs.java
+++ observer/src/main/java/com/iluwatar/observer/Orcs.java
@@ -31,7 +31,7 @@ import org.slf4j.LoggerFactory;
  *
  */
 public class Orcs implements WeatherObserver {
-
+  public static final int FOO = 0;
   private static final Logger LOGGER = LoggerFactory.getLogger(Orcs.class);
 
   @Override
